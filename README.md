7a2cb82db2eec17a0e772e986811d6c305b32ee9
Programmerid <v.mike.id@gmail.com>
2023-06-13 16:02:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+react-with-webpack-template
+Template for react with webpack config and backend server for bundle.
+
+Start project
+npm run dev#
---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './scss/main/main';
+
+const App = () => {
+    return (
+        <div className={styles.main}>
+
+            <header className={styles.header}>
+
+            </header>
+
+            <main className={styles.main_content}>
+
+            </main>
+
+            <footer className={styles.footer}>
+
+            </footer>
+
+        </div>
+    )
+}
+
+export default App
